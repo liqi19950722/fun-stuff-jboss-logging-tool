6cd4b204a17b0852939b6d8d97b877aa12a720f3
qi.li <15868175516@163.com>
2023-05-02 15:52:22 +0800

    refactor: delete

--- src/main/java/org/zucc/lq/fun/stuff/logging/example/logger/LogDemo.java
+++
@@ -1,7 +0,0 @@
-package org.zucc.lq.fun.stuff.logging.example.logger;
-
-public class LogDemo {
-    public static void main(String[] args) {
-        AppLogger.LOGGER.appVersion("fun stuff jboss logging", 0,0,1,"SNAPSHOT");
-    }
-}
